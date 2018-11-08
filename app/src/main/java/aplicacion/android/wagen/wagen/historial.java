e9d5c145f69f8683691523b08e7f503c7df734f1
Jesus ALejandro Reynoso Mendoza <reynoso_jesus@outlook.com>
2018-11-08 05:22:10 -0700

    aa

--- app/src/main/java/aplicacion/android/wagen/wagen/historial.java
+++ app/src/main/java/aplicacion/android/wagen/wagen/historial.java
@@ -52,26 +52,6 @@ public class historial extends FragmentActivity implements ActionBar.TabListener
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historial);
 
-        /*final android.app.ActionBar ab = getActionBar();
-        ab.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-
-        ab.addTab(ab.newTab().setText("Activos").setTabListener(this));*/
-
-        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-
-        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));*/
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
