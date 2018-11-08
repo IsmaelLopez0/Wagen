08b021a33f25dc075bbd9cef83de9a34d4a273d2
Ismael Lopez <Ismael_Lopez@outlook.com>
2018-11-08 11:51:45 -0700

    Cambio

---
+++ app/src/main/java/aplicacion/android/wagen/wagen/piedidosActuales.java
@@ -0,0 +1,13 @@
+package aplicacion.android.wagen.wagen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class piedidosActuales extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_piedidos_actuales);
+    }
+}
