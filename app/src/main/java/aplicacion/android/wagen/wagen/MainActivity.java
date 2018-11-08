388ee47400fbc9efab3494e8cc55f45d01bc04e1
Jesus ALejandro Reynoso Mendoza <reynoso_jesus@outlook.com>
2018-11-07 19:53:45 -0700

    pushDiseños

--- app/src/main/java/aplicacion/android/wagen/wagen/MainActivity.java
+++ app/src/main/java/aplicacion/android/wagen/wagen/MainActivity.java
@@ -2,12 +2,16 @@ package aplicacion.android.wagen.wagen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button BtnCrearC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
     }
 }
