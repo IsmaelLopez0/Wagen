d0d673f9221fa7cf3521ffe3f72ea024643f21b7
Jesus ALejandro Reynoso Mendoza <reynoso_jesus@outlook.com>
2018-11-07 20:32:58 -0700

    si nos dejan

--- app/src/main/java/aplicacion/android/wagen/wagen/MainActivity.java
+++ app/src/main/java/aplicacion/android/wagen/wagen/MainActivity.java
@@ -1,7 +1,9 @@
 package aplicacion.android.wagen.wagen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BtnCrearC = findViewById(R.id.btnCrearCuenta);
 
-        
+        BtnCrearC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirCrear();
+            }
+        });
+
+    }
+
+    private void abrirCrear(){
+        Intent intent = new Intent(getApplicationContext(), CrearCuenta.class);
+        startActivity(intent);
     }
 }
